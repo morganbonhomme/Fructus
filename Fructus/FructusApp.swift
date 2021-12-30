daf34b026f84d7a3a5a555ed9526c2e83af99a28
morganbonhomme <mbonhomme@lucca.fr>
2021-12-30 10:15:26 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Morgan Bonhomme on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
